43538c1e46f35a04c77adf5fa951b9e534b9a4c9
Richard Edgar <freesurfer.rge@gmail.com>
2026-02-21 20:06:27 -0500

    Working on things

--- nqcc/__main__.py
+++ nqcc/__main__.py
@@ -1,17 +1,30 @@
 import argparse
 
+
 def parse_args():
     parser = argparse.ArgumentParser(
-        prog = "nqcc",
-        description = "An implementation of the C Compiler described in Nora Sandler's book Writing a C Compiler",
-        add_help=True
+        prog="nqcc",
+        description="An implementation of the C Compiler described in Nora Sandler's book Writing a C Compiler",
+        add_help=True,
+    )
+
+    parser.add_argument("--lex", action="store_true", help="Only run the lexer then exit")
+
+    parser.add_argument(
+        "--parse", action="store_true", help="Only run the parser then exit (implies --lex)"
     )
 
     args = parser.parse_args()
+
+    if args.parse:
+        args.lex = True
+
     return args
+
 
 def main():
     args = parse_args()
+
 
 if __name__ == "__main__":
     main()
