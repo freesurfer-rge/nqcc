5abff36209b9153f00bd5650df9860b2588e1786
R Edgar <freesurfer.rge@gmail.com>
2026-02-21 21:03:41 -0500

    [FEAT] Basic driver (#2)
    A minimalist driver program for the compiler, designed to comply with the book specifications (with a bit of support for extension in the future).

---
+++ nqcc/__main__.py
@@ -0,0 +1,78 @@
+import argparse
+import logging
+import pathlib
+import shutil
+
+from nqcc import preprocess_c_file
+
+_DESC = """\
+An implementation of the C Compiler described in Nora 
+Sandler's book "Writing a C Compiler."
+
+The preprocessor will always be run.
+"""
+
+_DEFAULT_WORKING_DIR = "temp-nqcc"
+
+_WORKING_DIR_DESC = f"""\
+Directory to store intermediate files. By default
+this will be '{_DEFAULT_WORKING_DIR}' in the same
+directory as the target file. This directory will
+be DELETED and recreated before any other tooling
+runs.
+"""
+
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger("nqcc")
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="nqcc",
+        description=_DESC,
+        add_help=True,
+    )
+
+    book_group = parser.add_argument_group(title="Required for book's test suite")
+    book_group.add_argument("--lex", action="store_true", help="Only run the lexer then exit")
+    book_group.add_argument(
+        "--parse", action="store_true", help="Only run the parser then exit (implies --lex)"
+    )
+    book_group.add_argument(
+        "--codegen",
+        action="store_true",
+        help="Only run the code generator then exit (implies --lex and --parse)",
+    )
+
+    parser.add_argument("--working-dir", type=pathlib.Path, required=False, help=_WORKING_DIR_DESC)
+    parser.add_argument("target", type=pathlib.Path, help="Path to target C file")
+
+    args = parser.parse_args()
+
+    if args.codegen:
+        args.parse = True
+    if args.parse:
+        args.lex = True
+
+    if not args.working_dir:
+        args.working_dir = args.target.parent / _DEFAULT_WORKING_DIR
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    assert args.target.exists(), f"Target {args.target} does not exist!"
+
+    if args.working_dir.exists():
+        _logger.warning(f"Deleting working directory {args.working_dir}")
+        shutil.rmtree(args.working_dir)
+    args.working_dir.mkdir()
+
+    _logger.info("Running preprocessor")
+    preprocessed_file = preprocess_c_file(args.target, args.working_dir)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ nqcc/_preprocess.py
@@ -0,0 +1,16 @@
+import pathlib
+import subprocess
+
+PREPROCESS_EXTENSION = ".i"
+
+def preprocess_c_file(target_path: pathlib.Path, working_dir: pathlib.Path) -> pathlib.Path:
+    assert target_path.exists(), f"{target_path} not found"
+    assert working_dir.exists(), f"{working_dir} not found"
+
+    output_file_name = target_path.name + PREPROCESS_EXTENSION
+    output_path = working_dir / output_file_name
+
+    preprocess_command = [ "gcc", "-E", "-P", target_path, "-o", output_path]
+    subprocess.run(preprocess_command, check=True, timeout=5)
+
+    return output_path
