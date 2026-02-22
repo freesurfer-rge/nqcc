955d381ada827745c969e6ce0bb884617a6b95f0
Richard Edgar <freesurfer.rge@gmail.com>
2026-02-21 19:54:03 -0500

    Start draft of main

--- nqcc/__main__.py
+++ nqcc/__main__.py
@@ -1,4 +1,17 @@
 import argparse
 
 def parse_args():
-    
+    parser = argparse.ArgumentParser(
+        prog = "nqcc",
+        description = "An implementation of the C Compiler described in Nora Sandler's book Writing a C Compiler",
+        add_help=True
+    )
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+
+if __name__ == "__main__":
+    main()
