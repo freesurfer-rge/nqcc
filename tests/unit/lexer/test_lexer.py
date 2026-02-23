55e6b7f920ee8c35f81da8ece1597b2f640c8631
Richard Edgar <freesurfer.rge@gmail.com>
2026-02-23 18:16:13 -0500

    Expanding positive tests

--- tests/unit/lexer/test_lexer.py
+++ tests/unit/lexer/test_lexer.py
@@ -1,13 +1,25 @@
 from nqcc.lexer import (
+    CloseBraceToken,
     CloseParenToken,
+    ConstantIntegerToken,
     IdentifierToken,
     KeywordToken,
     Lexer,
+    OpenBraceToken,
     OpenParenToken,
+    SemicolonToken,
+    TokenItem
 )
 
 
 class TestLexer:
+    def compare_tokens_without_position(self, expected: list[TokenItem], actual: list[TokenItem]):
+        assert len(expected) == len(actual)
+
+        for (t_e, t_a) in zip(expected, actual):
+            assert type(t_e) == type(t_a)
+            assert t_e.value == t_a.value
+
     def test_smoke(self):
         target = Lexer()
 
@@ -28,6 +40,32 @@ class TestLexer:
             CloseParenToken(start_position=17, value=")"),
         ]
         assert final_tokens == expected_tokens
+
+    def test_simple_program(self):
+        target = Lexer()
+
+        program_str = "int my_func ( void ) { return 2; }"
+
+        expected_tokens = [
+            KeywordToken(value="int"),
+            IdentifierToken(value="my_func"),
+            OpenParenToken(value="("),
+            KeywordToken(value="void"),
+            CloseParenToken(value=")"),
+            OpenBraceToken(value="{"),
+            KeywordToken(value="return"),
+            ConstantIntegerToken(value="2"),
+            SemicolonToken(value=";"),
+            CloseBraceToken(value="}")
+        ]
+
+        for ch in program_str:
+            target.push_character(ch)
+        target.character_stream_done()
+
+        final_tokens = target.completed_token_list
+        self.compare_tokens_without_position(expected_tokens, final_tokens)
+
 
 class TestLexerFailures:
     def test_bad_identifier(self):
