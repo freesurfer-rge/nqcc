734c06e5943478baca57c8954b9a7f1d9c698f84
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-01 13:42:59 -0500

    One more

--- tests/unit/codegen/test_assembler_ast.py
+++ tests/unit/codegen/test_assembler_ast.py
@@ -3,10 +3,12 @@ from nqcc.codegen import (
     AsmMovNode,
     AsmRegisterNode,
     AsmRetNode,
+    AsmFunctionNode,
     convert_expression_node,
     convert_statement_node,
+    convert_function_node,
 )
-from nqcc.parser import SourceConstantIntNode, SourceReturnNode
+from nqcc.parser import SourceConstantIntNode, SourceReturnNode, TokenTape, parse_function
 
 
 class TestConvertExpressions:
@@ -42,3 +44,27 @@ class TestConvertStatementNode:
         instr1 = result[1]
         assert isinstance(instr1, AsmRetNode)
         assert instr1.start_position == 0
+
+
+class TestConvertFunctionNode:
+    def test_smoke(self):
+        program_str = "int main( void ) { return 45;}"
+
+        token_tape = TokenTape.from_c_source(program_str)
+
+        node = parse_function(token_tape)
+
+        result = convert_function_node(node)
+
+        assert isinstance(result, AsmFunctionNode)
+        assert result.identifier == "main"
+        assert result.start_position == 0
+        assert len(result.instructions) == 2
+
+        instr0 = result.instructions[0]
+        assert isinstance(instr0, AsmMovNode)
+        assert instr0.start_position == 19
+        assert instr0.source == AsmImmediateIntNode(start_position=26, value=45)
+        assert instr0.destination == AsmRegisterNode(start_position=26, value="eax")
+
+        assert result.instructions[1] == AsmRetNode(start_position=19)
