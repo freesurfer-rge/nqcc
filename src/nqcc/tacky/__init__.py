b7c2bdfbc7a1e81047328a1548b6dacbe7ecd16b
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-04 20:58:21 -0500

    Starting on the AST

---
+++ src/nqcc/tacky/__init__.py
@@ -0,0 +1,15 @@
+from ._tacky_ast import (
+    TackyASTNode,
+    TackyComplementNode,
+    TackyConstantIntNode,
+    TackyNegateNode,
+    TackyVarNode,
+)
+
+__all__ = [
+    "TackyASTNode",
+    "TackyComplementNode",
+    "TackyConstantIntNode",
+    "TackyNegateNode",
+    "TackyVarNode",
+]
--- src/nqcc/tacky/_tacky_ast.py
+++ src/nqcc/tacky/_tacky_ast.py
@@ -6,3 +6,24 @@ from pydantic import BaseModel
 class TackyASTNode(BaseModel):
     node_type: str
     start_position: int
+
+
+class TackyConstantIntNode(TackyASTNode):
+    node_type: Literal["TackyConstantIntNode"] = "TackyConstantIntNode"
+    value: int
+
+
+class TackyVarNode(TackyASTNode):
+    node_type: Literal["TackyVarNode"] = "TackyVarNode"
+    identifier: str
+
+
+class TackyComplementNode(TackyASTNode):
+    node_type: Literal["TackyComplementNode"] = "TackyComplementNode"
+
+
+class TackyNegateNode(TackyASTNode):
+    node_type: Literal["TackyNegateNode"] = "TackyNegateNode"
+
+
+TackyUnaryOperator = Union[TackyComplementNode, TackyNegateNode]
