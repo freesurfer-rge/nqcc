d0a2a01c549a478742a7344f3eac6ad1136d79cf
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-04 21:05:04 -0500

    Drafting the Tacky AST

--- src/nqcc/tacky/_tacky_ast.py
+++ src/nqcc/tacky/_tacky_ast.py
@@ -17,6 +17,7 @@ class TackyVarNode(TackyASTNode):
     node_type: Literal["TackyVarNode"] = "TackyVarNode"
     identifier: str
 
+TackyValue = Union[TackyConstantIntNode, TackyVarNode]
 
 class TackyComplementNode(TackyASTNode):
     node_type: Literal["TackyComplementNode"] = "TackyComplementNode"
@@ -27,3 +28,26 @@ class TackyNegateNode(TackyASTNode):
 
 
 TackyUnaryOperator = Union[TackyComplementNode, TackyNegateNode]
+
+
+class TackyReturnNode(TackyASTNode):
+    node_type: Literal["TackyReturnNode"] = "TackyReturnNode"
+    value: TackyValue
+
+class TackyUnaryNode(TackyASTNode):
+    node_type: Literal["TackUnaryNode"] = "TackUnaryNode"
+    operator: TackyUnaryOperator
+    src: TackyValue
+    dst: TackyValue
+
+TackyInstruction = Union[TackyReturnNode, TackyUnaryNode]
+
+class TackyFunctionNode(TackyASTNode):
+    node_type: Literal["TackyFunctionNode"] = "TackyFunctionNode"
+    identifier: str
+    instructions: list[TackyInstruction]
+
+class TackyProgramNode(TackyASTNode):
+    node_type: Literal["TackyProgramNode"] = "TackyProgramNode"
+    identifier: str
+    function_definition: TackyFunctionNode
