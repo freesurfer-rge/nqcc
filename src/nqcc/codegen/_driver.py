60283d6e5455a45fb4e4f0c3ace44ab4164658b5
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-08 16:12:45 -0400

    Hooking together a little

--- src/nqcc/codegen/_driver.py
+++ src/nqcc/codegen/_driver.py
@@ -1,21 +1,21 @@
 import pathlib
 
-from nqcc.codegen import AsmProgramNode, convert_program_node
-from nqcc.parser import SourceProgramNode
+from nqcc.codegen import AsmProgramNode, convert_tacky_program
+from nqcc.tacky import TackyProgramNode
 
 ASM_AST_EXTENSION = ".asm_ast"
 
 
 def codegen_driver(
-    source_ast: SourceProgramNode, *, working_dir: pathlib.Path, file_stem: str
+    source_ast: TackyProgramNode, *, working_dir: pathlib.Path, file_stem: str
 ) -> AsmProgramNode:
     assert working_dir.exists(), f"Unable to find working directory {working_dir}"
 
-    asm_ast = convert_program_node(source_ast)
+    asm_ast_0 = convert_tacky_program(source_ast)
 
-    output_file = file_stem + ASM_AST_EXTENSION
+    output_file = file_stem + ".0" + ASM_AST_EXTENSION
     output_path = working_dir / output_file
     with open(output_path, "w") as of:
-        of.write(asm_ast.model_dump_json(indent=4, round_trip=True))
+        of.write(asm_ast_0.model_dump_json(indent=4, round_trip=True))
 
-    return asm_ast
+    return asm_ast_0
