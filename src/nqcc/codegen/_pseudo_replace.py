b7af7a89626be43ea3bb95a8b5a9635eebfd2e37
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-08 16:43:46 -0400

    Drafting....

---
+++ src/nqcc/codegen/_pseudo_replace.py
@@ -0,0 +1,78 @@
+from ._assembler_ast import (
+    AsmFunctionNode,
+    AsmImmediateIntNode,
+    AsmInstructionNode,
+    AsmMovNode,
+    AsmNegOperator,
+    AsmNotOperator,
+    AsmOperandNode,
+    AsmProgramNode,
+    AsmStackNode,
+    AsmPseudoRegisterNode,
+    AsmRegisterNode,
+    AsmRetNode,
+    AsmUnaryNode,
+    AsmUnaryOperator,
+    AsmOperandNode,
+    AsmAllocateStackNode
+)
+
+FIRST_STACK_OFFSET = -4
+STACK_DELTA = 4
+
+class PseudoRegisterReplacer:
+    def __init__(self) -> None:
+        self._reset()
+
+    def _reset(self) -> None:
+        self.pseudo_map: dict[str, AsmStackNode] = {}
+        self.curr_offset = FIRST_STACK_OFFSET
+
+    def get_updated_operand(self, operand: AsmOperandNode) -> AsmOperandNode:
+        match operand:
+            case AsmImmediateIntNode():
+                return operand
+            case AsmRegisterNode():
+                return operand
+            case AsmPseudoRegisterNode():
+                if operand.identifier in self.pseudo_map:
+                    return self.pseudo_map[operand.identifier]
+                result = AsmStackNode(start_position=operand.start_position, offset=self.curr_offset)
+                self.curr_offset -= STACK_DELTA
+                self.pseudo_map[operand.identifier] = result
+                return result
+            case AsmStackNode():
+                # Should we log a warning?
+                return operand
+            case _:
+                raise ValueError(f"Unrecognised: {operand}")
+
+    def update_instruction(self, asm_instr: AsmInstructionNode):
+        match asm_instr:
+            case AsmMovNode():
+                asm_instr.source = self.get_updated_operand(asm_instr.source)
+                asm_instr.destination = self.get_updated_operand(asm_instr.destination)
+            case AsmUnaryNode():
+                asm_instr.source = self.get_updated_operand(asm_instr.source)
+            case AsmRetNode():
+                return
+            case AsmAllocateStackNode():
+                # Another possible place to log a warning
+                return
+            case _:
+                raise ValueError(f"Unrecognised {asm_instr}")
+
+    def pseudo_replace_function(self, asm_func: AsmFunctionNode):
+        assert isinstance(asm_func, AsmFunctionNode)
+        self._reset()
+
+        for instr in asm_func.instructions:
+            if isinstance(instr, AsmInstructionNode):
+                self.update_instruction(instr)
+            else:
+                pass
+
+    def pseudo_replace(self, asm_program: AsmProgramNode):
+        assert isinstance(asm_program, AsmProgramNode)
+
+        self.pseudo_replace_function(asm_program.function_definition)
