5409f2c0aed6192343611dbc1c27c944f2866840
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-09 22:10:45 -0400

    Updating

--- src/nqcc/_code_emission.py
+++ src/nqcc/_code_emission.py
@@ -9,6 +9,9 @@ from nqcc.codegen import (
     AsmProgramNode,
     AsmRegisterNode,
     AsmRetNode,
+    AsmStackNode,
+    AsmAllocateStackNode,
+    AsmNegOperator, AsmNotOperator, AsmUnaryNode, AsmUnaryOperator
 )
 
 ASSEMBLY_EXTENSION = ".s"
@@ -26,21 +29,57 @@ def get_operand_assembler(operand_node: AsmOperandNode) -> str:
             return f"${operand_node.value}"
         case v if isinstance(v, AsmRegisterNode):
             return f"%{operand_node.value}"
+        case v if isinstance(v, AsmStackNode):
+            return f"{operand_node.offset}(%rbp)"
         case _:
             raise ValueError(f"Unrecognised: {operand_node}")
 
 
+def get_unary_opcode(unary_operator: AsmUnaryOperator) -> str:
+    match unary_operator:
+        case AsmNegOperator():
+            return "negl"
+        case AsmNotOperator():
+            return "notl"
+        case _:
+            raise ValueError(f"Unrecognised: {unary_operator}")
+    
+
 def get_instruction_assembler(instr_node: AsmInstructionNode) -> str:
     match instr_node:
+        case AsmAllocateStackNode():
+            opcode = "subq".ljust(_OPCODE_FIELD_WIDTH)
+            src = f"${instr_node.stack_size}"
+            dst = r"%rsp"
+            return f"{opcode} {src}, {dst}"
         case v if isinstance(v, AsmMovNode):
             opcode = "movl".ljust(_OPCODE_FIELD_WIDTH)
             src = get_operand_assembler(instr_node.source)
             dst = get_operand_assembler(instr_node.destination)
-            return f"{opcode}{src}, {dst}"
+            return f"{opcode} {src}, {dst}"
         case v if isinstance(v, AsmRetNode):
             return "ret"
+        case AsmUnaryNode():
+            opcode = get_unary_opcode(instr_node.operator).ljust(_OPCODE_FIELD_WIDTH)
+            src = get_operand_assembler(instr_node.source)
+            return f"{opcode} {src}"
         case _:
             raise ValueError(f"Unrecognised: {instr_node}")
+
+
+def stack_setup() -> list[str]:
+    op0 = "pushq".ljust(_OPCODE_FIELD_WIDTH)
+    i0 = f"{_INSTRUCTION_INDENT}{op0} %rbp"
+    op1 = "movq".ljust(_OPCODE_FIELD_WIDTH)
+    i1 = f"{_INSTRUCTION_INDENT}{op1} %rsp, %rbp"
+    return [i0, i1]
+
+def stack_teardown() -> list[str]:
+    op0 = "movq".ljust(_OPCODE_FIELD_WIDTH)
+    i0 = f"{_INSTRUCTION_INDENT}{op0} %rbp, %rsp"
+    op1 = "popq".ljust(_OPCODE_FIELD_WIDTH)
+    i1 = f"{_INSTRUCTION_INDENT}{op1} %rbp"
+    return [i0, i1]
 
 
 def get_function_assembler(func_node: AsmFunctionNode) -> list[str]:
@@ -48,8 +87,14 @@ def get_function_assembler(func_node: AsmFunctionNode) -> list[str]:
     result.append(f"# Starting {func_node.identifier} ".ljust(_SEP_WIDTH, _SEP_CHAR))
     result.append(f"{_INSTRUCTION_INDENT}.globl {func_node.identifier}")
     result.append(f"{func_node.identifier}:")
+
+    result += stack_setup()
+
     for instr in func_node.instructions:
         nxt = get_instruction_assembler(instr)
+        if nxt == "ret":
+            # This could get troublesome if 'ret' isn't the last thing
+            result += stack_teardown()
         result.append(f"{_INSTRUCTION_INDENT}{nxt}")
     return result
 
