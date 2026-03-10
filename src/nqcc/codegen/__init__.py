f9633b60d9ed31940789561f42d9f9ce593d09b7
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-09 21:44:58 -0400

    Exports

--- src/nqcc/codegen/__init__.py
+++ src/nqcc/codegen/__init__.py
@@ -24,7 +24,7 @@ from ._convert_tacky import (
     convert_tacky_unary_operator,
 )
 from ._driver import codegen_driver
-from ._instruction_fixup import fixup_function_instructions, apply_mov_fixup
+from ._instruction_fixup import apply_mov_fixup, fixup_function_instructions
 from ._pseudo_replace import PseudoRegisterReplacer
 
 __all__ = [
@@ -45,12 +45,12 @@ __all__ = [
     "AsmUnaryNode",
     "AsmUnaryOperator",
     "PseudoRegisterReplacer",
+    "apply_mov_fixup",
     "codegen_driver",
     "convert_tacky_function",
     "convert_tacky_instruction",
     "convert_tacky_operand",
     "convert_tacky_program",
     "convert_tacky_unary_operator",
-    "apply_mov_fixup",
     "fixup_function_instructions",
 ]
