c0ed552445f24e4ef6ae52cfe18bac79d53d996a
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-09 22:18:23 -0400

    Working on test

---
+++ sample_programs/return_negative_constant.c
@@ -0,0 +1,5 @@
+#define RET_VAL 10
+
+int main(void) {
+    return -((RET_VAL));
+}
--- tests/integration/test_sample_programs.py
+++ tests/integration/test_sample_programs.py
@@ -7,10 +7,7 @@ from nqcc import __main__ as compiler_driver
 SAMPLE_PROGRAM_DIR = pathlib.Path(__file__).resolve().parent.parent.parent / "sample_programs"
 assert SAMPLE_PROGRAM_DIR.exists(), f"{SAMPLE_PROGRAM_DIR=} not found!"
 
-
-def test_return_constant():
-    target_file = SAMPLE_PROGRAM_DIR / "return_constant.c"
-
+def _compile_run_check(target_file: pathlib.Path, expected_return: int):
     with tempfile.TemporaryDirectory() as tmpdirname:
         working_dir = pathlib.Path(tmpdirname)
         executable_path = compiler_driver.main(
@@ -24,5 +21,16 @@ def test_return_constant():
         assert executable_path.exists(), f"Executable {executable_path} not generated!"
 
         result = subprocess.run([str(executable_path)], timeout=5)
-        assert result.returncode == 2
+        assert result.returncode == expected_return
     executable_path.unlink()
+
+
+def test_return_constant():
+    target_file = SAMPLE_PROGRAM_DIR / "return_constant.c"
+
+    _compile_run_check(target_file, 2)
+
+def test_return_negative_constant():
+    target_file = SAMPLE_PROGRAM_DIR / "return_negative_constant.c"
+
+    _compile_run_check(target_file, 246)
