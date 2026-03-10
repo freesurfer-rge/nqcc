9e47b8785a3c70dd56538ca26905ee83646857cf
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-09 21:51:55 -0400

    Hook in the fixup code

--- src/nqcc/codegen/_driver.py
+++ src/nqcc/codegen/_driver.py
@@ -1,12 +1,16 @@
+import logging
 import pathlib
 
 from nqcc.tacky import TackyProgramNode
 
 from ._assembler_ast import AsmProgramNode
 from ._convert_tacky import convert_tacky_program
+from ._instruction_fixup import fixup_program_instructions
 from ._pseudo_replace import PseudoRegisterReplacer
 
 ASM_AST_EXTENSION = ".asm_ast"
+
+_logger = logging.getLogger("nqcc")
 
 
 def codegen_driver(
@@ -14,6 +18,7 @@ def codegen_driver(
 ) -> AsmProgramNode:
     assert working_dir.exists(), f"Unable to find working directory {working_dir}"
 
+    _logger.info("Converting from Tacky")
     asm_ast_0 = convert_tacky_program(source_ast)
 
     output_file = file_stem + ".0" + ASM_AST_EXTENSION
@@ -21,10 +26,19 @@ def codegen_driver(
     with open(output_path, "w") as of:
         of.write(asm_ast_0.model_dump_json(indent=4, round_trip=True))
 
+    _logger.info("Running Pseudoregister replacement")
     prr = PseudoRegisterReplacer()
     prr.pseudo_replace(asm_ast_0)
 
     output_file = file_stem + ".1" + ASM_AST_EXTENSION
+    output_path = working_dir / output_file
+    with open(output_path, "w") as of:
+        of.write(asm_ast_0.model_dump_json(indent=4, round_trip=True))
+
+    _logger.info("Fixing up instructions")
+    fixup_program_instructions(asm_ast_0)
+
+    output_file = file_stem + ".2" + ASM_AST_EXTENSION
     output_path = working_dir / output_file
     with open(output_path, "w") as of:
         of.write(asm_ast_0.model_dump_json(indent=4, round_trip=True))
