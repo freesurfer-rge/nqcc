3637852b3af05fb2def75f83ac34eddb00f11bbc
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-10 20:51:11 -0400

    More tweaking

--- src/nqcc/tacky/_driver.py
+++ src/nqcc/tacky/_driver.py
@@ -5,19 +5,18 @@ from nqcc.parser import SourceProgramNode
 from ._tacky_ast import TackyProgramNode
 from ._tacky_generator import TackyGenerator
 
-SOURCE_TACKY_EXTENSION = ".tacky_ast"
+TACKY_FILE = "tacky.ast"
 
 
 def tacky_driver(
-    source_program: SourceProgramNode, *, working_dir: pathlib.Path, file_stem: str
+    source_program: SourceProgramNode, *, working_dir: pathlib.Path
 ) -> TackyProgramNode:
     assert working_dir.exists(), f"Unable to find working directory {working_dir}"
 
     generator = TackyGenerator()
     tacky_ast = generator.emit_program(source_program)
 
-    output_file = file_stem + SOURCE_TACKY_EXTENSION
-    output_path = working_dir / output_file
+    output_path = working_dir / TACKY_FILE
     with open(output_path, "w") as of:
         of.write(tacky_ast.model_dump_json(indent=4, round_trip=True))
 
