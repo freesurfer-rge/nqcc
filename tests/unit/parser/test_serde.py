e2874f40951a409012a081a96ba379c93cb3ec63
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-10 20:24:19 -0400

    Create a (failing) test

---
+++ tests/unit/parser/test_serde.py
@@ -0,0 +1,13 @@
+from nqcc.parser import TokenTape, parse_program, SourceProgramNode
+
+class TestParserSerde:
+    def test_simple(self):
+        source = " int main(void) {return - (~   566);}"
+        token_tape = TokenTape.from_c_source(source)
+        src_node = parse_program(token_tape)
+
+        json_str = src_node.model_dump_json()
+
+        restored = SourceProgramNode.model_validate_json(json_str)
+
+        assert restored == src_node
