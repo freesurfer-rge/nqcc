b27df190359520ad9efd4464e779b398678a48c5
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-12 20:27:35 -0400

    Ruffen

--- src/nqcc/parser/_driver.py
+++ src/nqcc/parser/_driver.py
@@ -2,8 +2,8 @@ import pathlib
 
 from nqcc.lexer import Token
 
-from ._source_ast import SourceProgramNode
 from ._parsing import parse_program
+from ._source_ast import SourceProgramNode
 from ._token_tape import TokenTape
 
 SOURCE_AST_FILE = "source.ast"
