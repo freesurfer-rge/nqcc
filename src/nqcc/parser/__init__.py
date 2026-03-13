c3a2c2bb74d7f9f57e23464c423d5ada237c23ba
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-12 20:24:34 -0400

    Update init

--- src/nqcc/parser/__init__.py
+++ src/nqcc/parser/__init__.py
@@ -1,5 +1,11 @@
 from ._driver import parser_driver
 from ._exceptions import SourceASTBadTypeError, SourceASTBadValueError
+from ._parsing import (
+    parse_expression,
+    parse_function,
+    parse_program,
+    parse_statement,
+)
 from ._source_ast import (
     SourceASTNode,
     SourceComplementNode,
@@ -11,10 +17,6 @@ from ._source_ast import (
     SourceReturnNode,
     SourceStatementNode,
     SourceUnaryExpressionNode,
-    parse_expression,
-    parse_function,
-    parse_program,
-    parse_statement,
 )
 from ._token_tape import TokenTape
 
