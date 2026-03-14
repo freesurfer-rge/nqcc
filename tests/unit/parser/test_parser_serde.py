00b744d7a0123211ce9299d79243b3368d4eb2ee
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-13 22:17:24 -0400

    Extend the round trip testing

--- tests/unit/parser/test_parser_serde.py
+++ tests/unit/parser/test_parser_serde.py
@@ -1,14 +1,25 @@
 from nqcc.parser import SourceProgramNode, TokenTape, parse_program
 
 
+def _check_round_trip(c_source: str):
+    token_tape = TokenTape.from_c_source(c_source)
+    src_node = parse_program(token_tape)
+
+    json_str = src_node.model_dump_json()
+
+    restored = SourceProgramNode.model_validate_json(json_str)
+
+    assert restored == src_node
+
+
 class TestParserSerde:
     def test_simple(self):
         source = " int main(void) {return - (~   566);}"
-        token_tape = TokenTape.from_c_source(source)
-        src_node = parse_program(token_tape)
+        _check_round_trip(source)
 
-        json_str = src_node.model_dump_json()
-
-        restored = SourceProgramNode.model_validate_json(json_str)
-
-        assert restored == src_node
+    def test_arithmetic(self):
+        source = """ int main(void) {
+        return - (~   1+491*5-(-2)%8);
+        }
+        """
+        _check_round_trip(source)
