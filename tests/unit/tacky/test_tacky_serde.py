d6f7bb185a10446da6583de4520a2576bfbca20a
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-15 08:35:48 -0400

    Simplify

--- tests/unit/tacky/test_tacky_serde.py
+++ tests/unit/tacky/test_tacky_serde.py
@@ -19,17 +19,6 @@ class TestTackySerde:
     def test_simple(self):
         source = " int main(void) {return -  (  ~(566));}"
         self._round_trip(source)
-        token_tape = TokenTape.from_c_source(source)
-        src_node = parse_program(token_tape)
-
-        target = TackyGenerator()
-        orig = target.emit_program(src_node)
-
-        json_str = orig.model_dump_json()
-
-        restored = TackyProgramNode.model_validate_json(json_str)
-
-        assert restored == orig
 
     def test_binary(self):
         source = "int main  (void ) { return 3- -5 * ~9 % 8;}"
