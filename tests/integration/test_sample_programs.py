003aab22658a27da61bedd94fb323583107402c8
Richard Edgar <freesurfer.rge@gmail.com>
2026-03-15 15:25:46 -0400

    ruffen

--- tests/integration/test_sample_programs.py
+++ tests/integration/test_sample_programs.py
@@ -71,7 +71,7 @@ class TestChapter03:
     @pytest.mark.parametrize("v2", [1, 2, 37])
     @pytest.mark.parametrize("op0", ["+", "-", "*", "/", "%"])
     @pytest.mark.parametrize("op1", ["+", "-", "*", "/", "%"])
-    def test_two_operator(self, v0: int, v1: int, v2: int, op0:str, op1:str):
+    def test_two_operator(self, v0: int, v1: int, v2: int, op0: str, op1: str):
         target_file = SAMPLE_PROGRAM_DIR / TestChapter03.SUB_DIR / "two_operator.c"
         macros = [f"V0={v0}", f"V1={v1}", f"V2={v2}", f"OP0={op0}", f"OP1={op1}"]
 
