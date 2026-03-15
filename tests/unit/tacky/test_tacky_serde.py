0590d1941358d53f5c149471837c6afdfec3642f
R Edgar <freesurfer.rge@gmail.com>
2026-03-15 08:36:39 -0400

    [Test] Serialisation (#31)
    I have been neglecting the serialisation tests as binary operators are added; remedy this.

--- tests/unit/tacky/test_tacky_serde.py
+++ tests/unit/tacky/test_tacky_serde.py
@@ -3,8 +3,7 @@ from nqcc.tacky import TackyGenerator, TackyProgramNode
 
 
 class TestTackySerde:
-    def test_simple(self):
-        source = " int main(void) {return -  (  ~(566));}"
+    def _round_trip(self, source: str) -> None:
         token_tape = TokenTape.from_c_source(source)
         src_node = parse_program(token_tape)
 
@@ -16,3 +15,11 @@ class TestTackySerde:
         restored = TackyProgramNode.model_validate_json(json_str)
 
         assert restored == orig
+
+    def test_simple(self):
+        source = " int main(void) {return -  (  ~(566));}"
+        self._round_trip(source)
+
+    def test_binary(self):
+        source = "int main  (void ) { return 3- -5 * ~9 % 8;}"
+        self._round_trip(source)
