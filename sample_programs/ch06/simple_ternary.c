1d71d439c7165489261cf4c7da01cdc22bf35ba9
Richard Edgar <freesurfer.rge@gmail.com>
2026-04-08 22:06:38 -0400

    Start on some integration tests

---
+++ sample_programs/ch06/simple_ternary.c
@@ -0,0 +1,5 @@
+int main(void) {
+    int b = V1;
+    int a = V0 ? 2-b : 3;
+    return a*b;
+}
---
+++ tests/integration/test_samples_ch06.py
@@ -0,0 +1,16 @@
+import pytest
+
+from .utils import SAMPLE_PROGRAM_DIR, compile_run_check
+
+
+class TestChapter06:
+    SUB_DIR = "ch06"
+
+    @pytest.mark.parametrize("v0", [0, 1])
+    @pytest.mark.parametrize("v1", [0, 1, 5])
+    def test_simple_ternay(self, v0: int, v1: int):
+        target_file = SAMPLE_PROGRAM_DIR / TestChapter06.SUB_DIR / "simple_ternary.c"
+        assert target_file.exists(), f"{target_file} not found"
+
+        macros = [f"V0={v0}", f"V1={v1}"]
+        compile_run_check(target_file, macros=macros)
