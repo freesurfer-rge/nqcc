2ce4e6b87b52630730f19fed99333d66c6533d39
Richard Edgar <freesurfer.rge@gmail.com>
2026-04-12 13:55:52 -0400

    One more integration test

---
+++ sample_programs/ch07/deeply_nested_scope.c
@@ -0,0 +1,13 @@
+int main(void) {
+    int x=V0;
+    {
+        int x=V1;
+        if( x>1 ) {
+            x=V2;
+            int x = 4;
+            return x;
+        }
+        return x;
+    }
+    return x;
+}
--- tests/integration/test_samples_ch07.py
+++ tests/integration/test_samples_ch07.py
@@ -23,3 +23,13 @@ class TestChapter07:
 
         macros = [f"V0={v0}", f"V1={v1}", f"V2={v2}"]
         compile_run_check(target_file, macros=macros)
+
+    @pytest.mark.parametrize("v0", [0, 1, 2])
+    @pytest.mark.parametrize("v1", [0, 1, 2])
+    @pytest.mark.parametrize("v2", [0, 1, 2])
+    def test_deeply_nested_scope(self, v0: int, v1: int, v2: int):
+        target_file = SAMPLE_PROGRAM_DIR / TestChapter07.SUB_DIR / "deeply_nested_scope.c"
+        assert target_file.exists(), f"{target_file} not found"
+
+        macros = [f"V0={v0}", f"V1={v1}", f"V2={v2}"]
+        compile_run_check(target_file, macros=macros)
