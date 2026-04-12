b96c659be400ef73963a34ee0a6673290bbe8c58
Richard Edgar <freesurfer.rge@gmail.com>
2026-04-12 13:25:41 -0400

    Adding a test

--- tests/unit/semantic_analysis/test_variable_resolver.py
+++ tests/unit/semantic_analysis/test_variable_resolver.py
@@ -12,7 +12,7 @@ from nqcc.parser import (
     SourceReturnNode,
     SourceTernaryExpressonNode,
     SourceVarNode,
-    TokenTape,
+    TokenTape,SourceCompoundNode,
     parse_declaration,
     parse_expression,
     parse_function,
@@ -315,6 +315,52 @@ class TestFunction:
         assert isinstance(ret, SourceReturnNode)
         assert isinstance(ret.value, SourceVarNode)
         assert ret.value.identifier == "a.0"
+
+    def test_with_nested(self):
+        c_str = """int main( void ) {
+            int x=1;
+            {
+                int y = x+1;
+                int x = 2;
+            }
+            return x;
+        }
+        """
+        token_tape = TokenTape.from_c_source(c_str)
+        func = parse_function(token_tape)
+        assert token_tape.tokens_remaining == 0
+
+        updated = resolve_function(func)
+        assert updated.identifier == "main"
+        assert len(updated.body.items) == 3
+
+        decl0 = updated.body.items[0]
+        assert isinstance(decl0, SourceDeclarationNode)
+        assert isinstance(decl0.identifier, SourceVarNode)
+        assert decl0.identifier.identifier == "x.0"
+
+        compound0 = updated.body.items[1]
+        assert isinstance(compound0, SourceCompoundNode)
+        assert len(compound0.block.items) == 2
+
+        decl1 = compound0.block.items[0]
+        assert isinstance(decl1, SourceDeclarationNode)
+        assert isinstance(decl1.identifier, SourceVarNode)
+        assert decl1.identifier.identifier == "y.1"
+        assert isinstance(decl1.initial, SourceBinaryExpressionNode)
+        assert isinstance(decl1.initial.left, SourceVarNode)
+        assert decl1.initial.left.identifier == "x.0"
+
+        decl2 = compound0.block.items[1]
+        assert isinstance(decl2, SourceDeclarationNode)
+        assert isinstance(decl2.identifier, SourceVarNode)
+        assert decl2.identifier.identifier == "x.2"
+
+        ret = updated.body.items[2]
+        assert isinstance(ret, SourceReturnNode)
+        assert isinstance(ret.value, SourceVarNode)
+        assert ret.value.identifier == "x.0"
+
 
 
 class TestProgram:
