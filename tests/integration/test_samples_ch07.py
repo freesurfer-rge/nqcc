61836e4ba706a00f80ed7429f9abc4cb6ff0c3a7
Richard Edgar <freesurfer.rge@gmail.com>
2026-04-12 13:51:38 -0400

    Another integration test

--- tests/integration/test_samples_ch07.py
+++ tests/integration/test_samples_ch07.py
@@ -13,3 +13,13 @@ class TestChapter07:
 
         macros = [f"V0={v0}"]
         compile_run_check(target_file, macros=macros)
+
+    @pytest.mark.parametrize("v0", [1, 3, 7])
+    @pytest.mark.parametrize("v1", [2, 5, 11])
+    @pytest.mark.parametrize("v2", [1, 13, 17])
+    def test_nested_scope(self, v0: int, v1: int, v2: int):
+        target_file = SAMPLE_PROGRAM_DIR / TestChapter07.SUB_DIR / "nested_scope.c"
+        assert target_file.exists(), f"{target_file} not found"
+
+        macros = [f"V0={v0}", f"V1={v1}", f"V2={v2}"]
+        compile_run_check(target_file, macros=macros)
