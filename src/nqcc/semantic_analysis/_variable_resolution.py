cda9bac092722e1003824e4369f0242d53147a75
Richard Edgar <freesurfer.rge@gmail.com>
2026-04-14 20:34:20 -0400

    Make variable_map an argument

--- src/nqcc/semantic_analysis/_variable_resolution.py
+++ src/nqcc/semantic_analysis/_variable_resolution.py
@@ -46,18 +46,19 @@ def make_inner_variable_map(outer_map: dict[str, VariableInfo]) -> dict[str, Var
 
 class VariableResolver:
     def __init__(self) -> None:
-        self._variable_map: dict[str, VariableInfo] = {}
         self._counter = 0
 
-    def resolve_declaration(self, decl: SourceDeclarationNode):
+    def resolve_declaration(
+        self, decl: SourceDeclarationNode, variable_map: dict[str, VariableInfo]
+    ):
         assert isinstance(decl, SourceDeclarationNode)
 
         orig_name = decl.identifier.identifier
-        if orig_name in self._variable_map and self._variable_map[orig_name].defined_in_block:
+        if orig_name in variable_map and variable_map[orig_name].defined_in_block:
             raise SemanticAnalysisDuplicateDeclaration(decl=decl)
         unique_name = f"{orig_name}.{self._counter}"
         self._counter += 1
-        self._variable_map[orig_name] = VariableInfo(name=unique_name, defined_in_block=True)
+        variable_map[orig_name] = VariableInfo(name=unique_name, defined_in_block=True)
         nxt_init: SourceExpressionNode | None = None
         if decl.initial is not None:
             nxt_init = self.resolve_expression(decl.initial)
@@ -69,18 +70,20 @@ class VariableResolver:
             start_position=decl.start_position, identifier=nxt_var, initial=nxt_init
         )
 
-    def resolve_statement(self, stmt: SourceStatementNode) -> SourceStatementNode:
+    def resolve_statement(
+        self, stmt: SourceStatementNode, variable_map: dict[str, VariableInfo]
+    ) -> SourceStatementNode:
         sp = stmt.start_position
         match stmt:
             case SourceReturnNode():
-                updated = self.resolve_expression(stmt.value)
+                updated = self.resolve_expression(stmt.value, variable_map)
                 return SourceReturnNode(start_position=sp, value=updated)
             case SourceIfStatementNode():
-                cond_update = self.resolve_expression(stmt.condition)
-                then_update = self.resolve_statement(stmt.then)
+                cond_update = self.resolve_expression(stmt.condition, variable_map)
+                then_update = self.resolve_statement(stmt.then, variable_map)
                 otherwise_update = None
                 if stmt.otherwise:
-                    otherwise_update = self.resolve_statement(stmt.otherwise)
+                    otherwise_update = self.resolve_statement(stmt.otherwise, variable_map)
                 return SourceIfStatementNode(
                     start_position=sp,
                     condition=cond_update,
@@ -88,37 +91,34 @@ class VariableResolver:
                     otherwise=otherwise_update,
                 )
             case SourceExpressionStatementNode():
-                updated = self.resolve_expression(stmt.value)
+                updated = self.resolve_expression(stmt.value, variable_map)
                 return SourceExpressionStatementNode(start_position=sp, value=updated)
             case SourceNullStatementNode():
                 return SourceNullStatementNode(start_position=sp)
             case SourceCompoundNode():
-                inner_map = make_inner_variable_map(self._variable_map)
-                # This save/restore is ugly.....
-                orig_map = copy.deepcopy(self._variable_map)
-                self._variable_map = inner_map
-                resolved_block = self.resolve_block(stmt.block)
-                result = SourceCompoundNode(start_position=sp, block=resolved_block)
-                self._variable_map = orig_map
-                return result
+                inner_map = make_inner_variable_map(variable_map)
+                resolved_block = self.resolve_block(stmt.block, inner_map)
+                return SourceCompoundNode(start_position=sp, block=resolved_block)
             case _:
                 raise ValueError(f"Unrecognised: {stmt}")
 
-    def resolve_expression(self, expr: SourceExpressionNode) -> SourceExpressionNode:
+    def resolve_expression(
+        self, expr: SourceExpressionNode, variable_map: dict[str, VariableInfo]
+    ) -> SourceExpressionNode:
         match expr:
             case SourceAssignmentNode():
                 if not isinstance(expr.left, SourceVarNode):
                     raise SemanticAnalysisBadLValue(expr=expr.left)
                 return SourceAssignmentNode(
                     start_position=expr.start_position,
-                    left=self.resolve_expression(expr.left),
-                    right=self.resolve_expression(expr.right),
+                    left=self.resolve_expression(expr.left, variable_map),
+                    right=self.resolve_expression(expr.right, variable_map),
                 )
             case SourceVarNode():
                 if expr.identifier in self._variable_map:
                     return SourceVarNode(
                         start_position=expr.start_position,
-                        identifier=self._variable_map[expr.identifier].name,
+                        identifier=variable_map[expr.identifier].name,
                     )
                 else:
                     raise SemanticAnalysisUnknownVariable(var=expr)
@@ -128,49 +128,54 @@ class VariableResolver:
                 return SourceUnaryExpressionNode(
                     start_position=expr.start_position,
                     operator=expr.operator,
-                    expression=self.resolve_expression(expr.expression),
+                    expression=self.resolve_expression(expr.expression, variable_map),
                 )
             case SourceBinaryExpressionNode():
                 return SourceBinaryExpressionNode(
                     start_position=expr.start_position,
                     operator=expr.operator,
-                    left=self.resolve_expression(expr.left),
-                    right=self.resolve_expression(expr.right),
+                    left=self.resolve_expression(expr.left, variable_map),
+                    right=self.resolve_expression(expr.right, variable_map),
                 )
             case SourceTernaryExpressonNode():
                 return SourceTernaryExpressonNode(
                     start_position=expr.start_position,
-                    condition=self.resolve_expression(expr.condition),
-                    then=self.resolve_expression(expr.then),
-                    otherwise=self.resolve_expression(expr.otherwise),
+                    condition=self.resolve_expression(expr.condition, variable_map),
+                    then=self.resolve_expression(expr.then, variable_map),
+                    otherwise=self.resolve_expression(expr.otherwise, variable_map),
                 )
             case _:
                 raise ValueError(f"Not recognised: {expr}")
 
-    def resolve_block(self, block: SourceBlockNode) -> SourceBlockNode:
+    def resolve_block(
+        self, block: SourceBlockNode, variable_map: dict[str, VariableInfo]
+    ) -> SourceBlockNode:
         assert isinstance(block, SourceBlockNode)
 
         resolved_items: list[SourceBlockItemNode] = []
         for item in block.items:
-            nxt = self.resolve_blockitem(item)
+            nxt = self.resolve_blockitem(item, variable_map)
             resolved_items.append(nxt)
 
         return SourceBlockNode(start_position=block.start_position, items=resolved_items)
 
-    def resolve_blockitem(self, bi: SourceBlockItemNode) -> SourceBlockItemNode:
+    def resolve_blockitem(
+        self, bi: SourceBlockItemNode, variable_map: dict[str, VariableInfo]
+    ) -> SourceBlockItemNode:
 
         match bi:
             case SourceDeclarationNode():
-                return self.resolve_declaration(bi)
+                return self.resolve_declaration(bi, variable_map)
             case _ if isinstance(bi, get_args(SourceStatementNode)):
-                return self.resolve_statement(bi)
+                return self.resolve_statement(bi, variable_map)
             case _:
                 raise ValueError(f"Unrecognised: {bi}")
 
 
 def resolve_function(func: SourceFunctionNode) -> SourceFunctionNode:
     resolver = VariableResolver()
-    updated_body = resolver.resolve_block(func.body)
+    variable_map: dict[str, VariableInfo] = {}
+    updated_body = resolver.resolve_block(func.body, variable_map)
     result = SourceFunctionNode(
         start_position=func.start_position, identifier=func.identifier, body=updated_body
     )
