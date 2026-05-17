b19fa80e19f1aca27c6ca8ce2a8c0a4e8a10af18
Richard Edgar <freesurfer.rge@gmail.com>
2026-05-17 19:50:19 -0400

    Small test fix

--- tests/unit/codegen/test_instruction_fixup.py
+++ tests/unit/codegen/test_instruction_fixup.py
@@ -392,9 +392,9 @@ class TestProgramFixup:
             stack_size=8,
         )
 
-        target = AsmProgramNode(start_position=0, function_definition=func)
+        target = AsmProgramNode(start_position=0, functions=[func])
 
         fixup_program_instructions(target)
 
         # Just check that the expected increase has happened
-        assert len(target.function_definition.instructions) == 3
+        assert len(target.functions[0].instructions) == 3
