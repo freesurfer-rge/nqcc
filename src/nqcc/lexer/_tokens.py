a281145cbcd8b8390106f572167384dedd7b9326
Richard Edgar <freesurfer.rge@gmail.com>
2026-06-02 20:29:51 -0400

    Add keywords in src

--- src/nqcc/lexer/_tokens.py
+++ src/nqcc/lexer/_tokens.py
@@ -44,7 +44,7 @@ class ConstantIntegerToken(Token):
         return "[0-9]+\\b"
 
 
-_KEYWORDS = {"int", "return", "void", "if", "else", "do", "while", "for", "break", "continue"}
+_KEYWORDS = {"int", "return", "void", "if", "else", "do", "while", "for", "break", "continue", "extern", "static"}
 
 
 class KeywordToken(Token):
