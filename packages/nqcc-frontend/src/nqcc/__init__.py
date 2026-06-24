5cb190088ec00fdeb1bf9712e99e5b3c07829e4c
Richard Edgar <freesurfer.rge@gmail.com>
2026-06-23 21:31:37 -0400

    Fix the imports

--- packages/nqcc-frontend/src/nqcc/__init__.py
+++ packages/nqcc-frontend/src/nqcc/__init__.py
@@ -3,7 +3,3 @@
 # https://docs.python.org/3/reference/import_system.html#namespace-packages
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
-from ._code_emission import emit_assembler
-from ._invoke_gcc import generate_executable, generate_objectfile, preprocess_c_file
-
-__all__ = ["emit_assembler", "generate_executable", "generate_objectfile", "preprocess_c_file"]
